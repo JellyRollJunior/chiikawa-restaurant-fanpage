297a00bfce7109b90da23629cf915e42e0203ef1
Brandon Lin <brandonlin9716@gmail.com>
2024-09-04 23:44:14 -0700

    Configure image loading through Javascript

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
