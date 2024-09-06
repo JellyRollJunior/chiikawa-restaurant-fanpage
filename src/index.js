4c7b9f719e3ded749c38742b454aa3f11752fff1
Brandon Lin <brandonlin9716@gmail.com>
2024-09-05 18:27:10 -0700

    Render homepage on clicking nav homepage button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import "./styles.css";
 import { Homepage } from "./homepage.js";
 
-Homepage.renderHomepage();
+const TabController = (function() {
+    
+    // initial render
+    Homepage.renderHomepage();
+
+    const navigationMenu = document.querySelector("nav");
+    const ClickHandlerNavigationMenu = (event) => {
+        // clear content
+        const contentRoot = document.querySelector("div#content");
+        contentRoot.textContent = "";
+        const target = event.target;
+        switch(target.id) {
+            case "nav-home":
+                Homepage.renderHomepage();
+                break;
+            case "nav-menu":
+                break;
+            case "nav-contact":
+                break;
+            default:
+                Homepage.renderHomepage();
+        }
+    }
+    navigationMenu.addEventListener("click", ClickHandlerNavigationMenu);
+})();
